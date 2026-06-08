0581658259efed3811cf663e4f2eee2f5cc910e1
irem884 <iremulucan884@gmail.com>
2026-06-08 18:02:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+url="https://www.milligazete.com.tr/arsiv/2021-07-"
+def tarih_yaz(gun):
+    for i in range (1,gun+1,1):
+        if(i<10):
+            print(url+"0"+str(i))
+        else:
+            print(url+str(i))  
+
+tarih_yaz(30)   #İrem Ulucan tarafından kodlandı
+gulsenem="12:16 ABD ve İsrail İran'a saldırdı! Netanyahu'dan ilk açıklama"
+tkm=gulsenem.split(" ")
+print(tkm) 
